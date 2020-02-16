69db42b48e45a7320174ec52d9e657789a00afa0
slim-abid <52780461+slim-abid@users.noreply.github.com>
2020-02-16 01:50:08 +0100

    Add files via upload

---
+++ erpdisc.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en" >
+<link rel="shortcut icon" type="image/png" href="http://example.com/favicon.png"/>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<head>
+    <meta charset="UTF-8">
+    <title>DISC test</title>
+    <link rel="stylesheet" href="erpdisc.css">
+</head>
+<body>
+<div class="presentation_form">
+<span class="pres">Pour chacune des questions qui vont suivre, choisissez la proposition qui vous correspond le plus </span><span class="glyphicon glyphicon-plus-sign"></span> <span class="pres">et celle qui vous correspond le moins </span><span class="glyphicon glyphicon-minus-sign"></span><span class="pres">. Vous devez donc cocher deux cases par question.
+
+    Ne cherchez pas d'ordre dans les questions ou dans les réponses car il n'y en a pas.
+
+    </span>
+</div>
+<div>
+    <form method="post" action="adminlte-master/index.html">
+        <fieldset>
+    <table>
+
+<tr class="question">
+    <td >1. Parmi les métiers proposés, je choisirais celui de</td>
+    <td class="choix"><span class="glyphicon glyphicon-minus-sign"></span></td>
+    <td class="choix"><span class="glyphicon glyphicon-plus-sign"></span></td>
+
+</tr></br>
+<tr class="reponse  even">
+    <td>Infirmier, pour son dévouement aux autres</td>
+    <td class="choix"><input type="radio" name="rep_p4_q1" value="1" id="rep_p4_q1_l1" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q1" value="1" id="rep_p1_q1_l1" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>Entrepreneur, pour son sens du challenge</td>
+    <td class="choix"><input type="radio" name="rep_p4_q1" value="2" id="rep_p4_q1_l2" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q1" value="2" id="rep_p1_q1_l2" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  even">
+    <td>Comptable ou juriste, pour sa précision</td>
+    <td class="choix"><input type="radio" name="rep_p4_q1" value="3" id="rep_p4_q1_l3" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q1" value="3" id="rep_p1_q1_l3" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>Journaliste ou écrivain, pour son côté investigateur</td>
+    <td class="choix"><input type="radio" name="rep_p4_q1" value="4" id="rep_p4_q1_l4" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q1" value="4" id="rep_p1_q1_l4" class="alone alone-1"></td>
+</tr></br>
+<tr><td class="question-spacer"></td></tr></br>
+<tr class="question ">
+    <td>2. Ce qui me fait peur</td>
+    <td class="choix"><span class="glyphicon glyphicon-minus-sign"></span></td>
+    <td class="choix"><span class="glyphicon glyphicon-plus-sign"></span></td>
+
+</tr></br>
+<tr class="reponse  even">
+    <td>Qu'on critique mon travail</td>
+    <td class="choix"><input type="radio" name="rep_p4_q2" value="1" id="rep_p4_q2_l1" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q2" value="1" id="rep_p1_q2_l1" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>Qu'on profite de moi</td>
+    <td class="choix"><input type="radio" name="rep_p4_q2" value="2" id="rep_p4_q2_l2" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q2" value="2" id="rep_p1_q2_l2" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  even">
+    <td>Qu'on me presse d'aller vite</td>
+    <td class="choix"><input type="radio" name="rep_p4_q2" value="3" id="rep_p4_q2_l3" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q2" value="3" id="rep_p1_q2_l3" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>Qu'on me rejette</td>
+    <td class="choix"><input type="radio" name="rep_p4_q2" value="4" id="rep_p4_q2_l4" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q2" value="4" id="rep_p1_q2_l4" class="alone alone-1"></td>
+</tr></br>
+
+
+
+<tr><td class="question-spacer"></td></tr></br>
+<tr class="question ">
+    <td>3. C'est dimanche, j'ai prévu</td>
+    <td class="choix"><span class="glyphicon glyphicon-minus-sign"></span></td>
+    <td class="choix"><span class="glyphicon glyphicon-plus-sign"></span></td>
+
+</tr>
+</br>
+<tr class="reponse  even">
+    <td>D'organiser une petite fête avec les amis et voisins</td>
+    <td class="choix"><input type="radio" name="rep_p4_q3" value="1" id="rep_p4_q3_l1" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q3" value="1" id="rep_p1_q3_l1" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>De faire ce que j'aime sans m'occuper des autres</td>
+    <td class="choix"><input type="radio" name="rep_p4_q3" value="2" id="rep_p4_q3_l2" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q3" value="2" id="rep_p1_q3_l2" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  even">
+    <td>De m'occuper de ceux qui ont besoin d'aide</td>
+    <td class="choix"><input type="radio" name="rep_p4_q3" value="3" id="rep_p4_q3_l3" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q3" value="3" id="rep_p1_q3_l3" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>De mettre de l'ordre dans mes papiers afin d'avoir l'esprit libre</td>
+    <td class="choix"><input type="radio" name="rep_p4_q3" value="4" id="rep_p4_q3_l4" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q3" value="4" id="rep_p1_q3_l4" class="alone alone-1"></td>
+</tr></br>
+
+
+
+<tr><td class="question-spacer"></td></tr></br>
+<tr class="question ">
+    <td>4. Ce qui me reflète le mieux</td>
+    <td class="choix"><span class="glyphicon glyphicon-minus-sign"></span></td>
+    <td class="choix"><span class="glyphicon glyphicon-plus-sign"></span></td>
+
+</tr></br>
+<tr class="reponse  even">
+    <td>J'aime les compliments, les éloges</td>
+    <td class="choix"><input type="radio" name="rep_p4_q4" value="1" id="rep_p4_q4_l1" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q4" value="1" id="rep_p1_q4_l1" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>Je suis bienveillant, prêt à donner ou à aider</td>
+    <td class="choix"><input type="radio" name="rep_p4_q4" value="2" id="rep_p4_q4_l2" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q4" value="2" id="rep_p1_q4_l2" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  even">
+    <td>Je suis formel et garde mes distances</td>
+    <td class="choix"><input type="radio" name="rep_p4_q4" value="3" id="rep_p4_q4_l3" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q4" value="3" id="rep_p1_q4_l3" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>J'ai de la force de caractère</td>
+    <td class="choix"><input type="radio" name="rep_p4_q4" value="4" id="rep_p4_q4_l4" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q4" value="4" id="rep_p1_q4_l4" class="alone alone-1"></td>
+</tr></br>
+
+
+
+<tr><td class="question-spacer"></td></tr></br>
+<tr class="question ">
+    <td>5. Vous préférez qu'on complimente</td>
+    <td class="choix"><span class="glyphicon glyphicon-minus-sign"></span></td>
+    <td class="choix"><span class="glyphicon glyphicon-plus-sign"></span></td>
+
+</tr></br>
+<tr class="reponse  even">
+    <td>Votre équipe</td>
+    <td class="choix"><input type="radio" name="rep_p4_q5" value="1" id="rep_p4_q5_l1" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q5" value="1" id="rep_p1_q5_l1" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>Vos accomplissements</td>
+    <td class="choix"><input type="radio" name="rep_p4_q5" value="2" id="rep_p4_q5_l2" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q5" value="2" id="rep_p1_q5_l2" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  even">
+    <td>Votre raisonnement</td>
+    <td class="choix"><input type="radio" name="rep_p4_q5" value="3" id="rep_p4_q5_l3" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q5" value="3" id="rep_p1_q5_l3" class="alone alone-1"></td>
+</tr></br>
+<tr class="reponse  odd">
+    <td>Vous-même, tout simplement</td>
+    <td class="choix"><input type="radio" name="rep_p4_q5" value="4" id="rep_p4_q5_l4" class="alone alone-4"></td>
+    <td class="choix"><input type="radio" name="rep_p1_q5" value="4" id="rep_p1_q5_l4" class="alone alone-1"></td>
+</tr></br>
+
+
+
+<tr>
+    </br><td class="question-spacer"></td></tr>
+</table>
+    <button type="submit" class="btn btn-primary">submit</button>
+        </fieldset>
+    </form>
+</div>
+
+</body>
+
+</html>
